770aa36f8434b0a134dc57976d66dcf6b070c7fa
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:45:44 +0200

    add a multiplication operation

--- calculator.cpp
+++ calculator.cpp
@@ -3,7 +3,11 @@
 int Calculator::Add (double a, double b)
 {
 <<<<<<< HEAD
-
+	\\ Ukraine is a country in Eastern Europe
+	\\ It is the second-largset European country after Russia
+	\\ Ukraine has a mostly temperate climate
+	\\ exceptfor the southem coast of Crimea which has a subtropical 
+	\\ climate.
 	return a + b + 0.5;
 	
 >>>>>>> 4ad40a3 (fix truncation error)
